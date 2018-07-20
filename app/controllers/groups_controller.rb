7bf93860420e1585d432e3f885df21f150b6b780
sagawa311 <harusuke311@gmail.com>
2018-07-20 18:47:20 +0900

    Change groups_controller
    Updateアクションをprivateメソッドの上に置いて編集後groupのメッセージページに飛ばせるようにしました。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,15 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
+
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
@@ -26,13 +35,6 @@ class GroupsController < ApplicationController
   def edit
   end
 
-  def update
-    if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-    else
-      render :edit
-    end
-  end
 
   private
   def group_params
