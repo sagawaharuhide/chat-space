56efd7820d833a43505c8997ba5e0f4958749962
sagawa311 <harusuke311@gmail.com>
2018-07-25 12:09:41 +0900

    Edit-message.js
    ・返ってきたJSONをdoneメソッドで受取り、HTMLを作成。
    ・HTMLを追加した分、メッセージ画面を下にスクロール

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,49 @@
 $(function(){
+  function buildHTML(message){
+    var img = message.image ? `<img src="${ message.image }">` : "";
+
+    var html = `<div class="message-contents__message-box">
+                  <div class="message-contents__message-box__upper-message">
+                    <div class="message-name">
+                      ${ message.name }
+                    </div>
+                    <div class="message-date">
+                      ${ message.created_at }
+                    </div>
+                  </div>
+                  <div class="message-contents__message-box__lower-message">
+                    <div class="message-content">
+                      ${ message.content }
+                    </div>
+                    <div class='message-contents__message-box__lower-message'>
+                      ${ img }
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
-      type: "POST"
+      type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
-  })
-})
+    .done(function(data){
+      var html = buildHTML(data);
+      console.log(html)
+      $('.message-contents__message-box').append(html);
+      $('.form__message').val('')
+      $('.form__submit').prop('disabled', false);
+      $('.message-contents').animate({scrollTop: $('.message-contents')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('error');
+    })
+  });
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
 json.content @message.content
-json.image @message.image
+json.image @message.image.url
 json.user_id @message.user_id
-json.user_name @message.user_name
+json.name @message.user.name
 json.created_at @message.created_at.in_time_zone('Tokyo').to_s
